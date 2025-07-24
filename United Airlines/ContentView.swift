e9b30237697afd92864c0fe7121b86a6d30d2296
Krishna Babani <57276165+babanikrishna@users.noreply.github.com>
2025-07-24 11:01:41 +0530

    Initial Commit

---
+++ United Airlines/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  United Airlines
+//
+//  Created by Krishna Babani on 7/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ United Airlines/United_AirlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  United_AirlinesApp.swift
+//  United Airlines
+//
+//  Created by Krishna Babani on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct United_AirlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
